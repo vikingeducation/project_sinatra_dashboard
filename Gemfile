5cb6b020a2743206c4cfa2657d454881df61d1e6
Dan Berger <dsberger@gmail.com>
2015-02-14 11:07:20 -0800

    working scraper to get weather results and calendar format

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'mechanize', '~>2.7.2', '<2.7.3'
+gem 'nokogiri'
 
 group :development do
   gem 'pry-byebug'
+  gem 'pry'
 end
--- helpers/weather_scraper.rb
+++ helpers/weather_scraper.rb
@@ -1,15 +1,19 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'mechanize'
 require 'nokogiri'
 require 'pry'
 
 
 class WeatherScraper
-  def initialize (location)
+  attr_reader :page, :calendar
+  def initialize(location)
     weatherman = Mechanize.new
-
-    page = weatherman.get('')
-
-
+    wunderground_url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=" + location.to_s
+    @page = weatherman.get(wunderground_url)
+    calendar_link = page.link_with text: " View Calendar Forecast"
+    calendar_url = calendar_link.href
+    @calendar = weatherman.get(calendar_url)
   end
 
   def get_weather_data
