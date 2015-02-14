c586ec0f98bd9b67637efacceb160d7661ae7d4a
Daniel Au <dakmau@gmail.com>
2015-02-14 14:26:55 -0500

    \_/(^_-)\_/ DO THE HUSTLE

---
+++ helpers/weather_day.rb
@@ -0,0 +1,1 @@
+WeatherDay = Struct.new(:date, :temperate, :classification)
--- helpers/weather_scraper.rb
+++ helpers/weather_scraper.rb
@@ -3,20 +3,31 @@ require 'bundler/setup'
 require 'mechanize'
 require 'nokogiri'
 require 'pry'
+require_relative 'weather_day.rb'
 
 
 class WeatherScraper
   attr_reader :page, :calendar
   def initialize(location)
-    weatherman = Mechanize.new
     wunderground_url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=" + location.to_s
+    weatherman = Mechanize.new
     @page = weatherman.get(wunderground_url)
     calendar_link = page.link_with text: " View Calendar Forecast"
     calendar_url = calendar_link.href
     @calendar = weatherman.get(calendar_url)
+    binding.pry
+
+    @forecast = []
+
+    calendar.search("td.day.todayBorder") # This breaks tings
+    calendar.search("td.day.forecast")[0].text # Also breaks
   end
 
   def get_weather_data
+
+  end
+
+  def scrape_forecast
 
   end
 end
