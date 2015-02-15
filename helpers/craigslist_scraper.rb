a41706b4f47e77ce1862068d0f3bace993ec5b38
Daniel Au <dakmau@gmail.com>
2015-02-14 19:39:13 -0500

    URL and title scraper

--- helpers/craigslist_scraper.rb
+++ helpers/craigslist_scraper.rb
@@ -1,7 +1,7 @@
 require 'mechanize'
 
 class CraigslistScraper
-  attr_reader :search_page, :scraper
+  attr_reader :search_page, :scraper, :results_page
   def initialize(min_price, max_price, keywords)
     @scraper = Mechanize.new
     @scraper.history_added = Proc.new { sleep 0.5 }
@@ -11,5 +11,29 @@ class CraigslistScraper
     form["maxAsk"] = max_price
     form["query"] = keywords
     @results_page = scraper.submit(form)
+    @results_nodes = @results_page.search("div.content p.row")
+  end
+
+  # def get_postings
+  #   (0..99).each_with_object([]) do |posting, classfield|
+  # end
+
+  def get_name(posting)
+    @results_nodes[posting].search("span.pl a").text.strip
+  end
+
+  def get_url(posting)
+    "http://sfbay.craigslist.org" + @results_nodes[posting].search("a.i").first[:href]
+  end
+
+  def get_price
+    # @results_nodes[posting].search("span.pl a").text.strip
+  end
+
+  def get_location
+    # @results_nodes[posting].search("span.pl a").text.strip
+  end
+
+  def get_email
   end
 end
