16f4545a98a9246b9aaa50e1727ed7f57da8d340
niconan <nick.hax@gmail.com>
2015-04-27 00:33:40 -0700

    craig scraper working w/ form inputs

--- helpers/craig_scrape.rb
+++ helpers/craig_scrape.rb
@@ -3,36 +3,40 @@ require 'mechanize'
 Listing = Struct.new(:name, :link, :email, :price, :location)
 
 class CraigScraper
-
-  def initialize
+  def initialize(min_ask, max_ask, keywords)
     @listings = []
+    @scraper = Mechanize.new
+    @scraper.history_added = proc { sleep 0.5 }
+    @min = min_ask
+    @max = max_ask
+    @keywords = keywords
   end
 
-  def get_listings
-
-    scraper = Mechanize.new do |agent|
-      agent.user_agent_alias = 'Mac Safari'
-      agent.history_added = Proc.new { sleep 0.5 } # just in case
-    end
-
-    scraper.get('http://sfbay.craigslist.org/apa') do |page|
-
-      page.links_with(href: /\/.{3}\/apa/).each do |link|
-
-        current_listing = Listing.new
-
-        current_listing.name = link.text.strip
-        current_listing.link = 'http://sfbay.craigslist.org' + link.uri.to_s
-
-        description_page = link.click
-
-        current_listing.price = description_page.search('.price').text.strip
-        current_listing.location = description_page.search('//*[@id="pagecontainer"]/section/h2/span[2]/small').text.gsub("(", "").gsub(")", "").strip
-
-        @listings << current_listing
-
-      end
-    end
+  def form_submit
+    @scraper.get('http://sfbay.craigslist.org/apa') do |page|
+      form = page.forms[1]
+      form['minAsk'] = @min
+      form['maxAsk'] = @max
+      form['query'] = @keywords
+      @results_page = form.submit
+    end # performing the search from the apts/housing page
   end
 
+  def listings
+    form_submit
+
+    @results_page.links_with(href: %r{\/.{3}\/apa}).each do |link|
+      current_listing = Listing.new
+
+      current_listing.name = link.text.strip
+      current_listing.link = 'http://sfbay.craigslist.org' + link.uri.to_s
+
+      description_page = link.click
+
+      current_listing.price = description_page.search('.price').text.strip
+      current_listing.location = description_page.search('//*[@id="pagecontainer"]/section/h2/span[2]/small').text.gsub("(", "").gsub(")", "").strip
+
+      @listings << current_listing
+    end
+  end
 end
