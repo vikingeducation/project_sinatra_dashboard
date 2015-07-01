b787465125fbe6bd51c64fe634c06073e03fe6d4
Adam <adam.kinson@gmail.com>
2015-07-01 08:24:03 -0400

    finish GlassDoor get_ratings and featured_review methods

--- company_profiler.rb
+++ company_profiler.rb
@@ -1,14 +1,15 @@
 class CompanyProfiler
+  # Example request ~ http://api.glassdoor.com/api/api.htm?t.p=38330&t.k=bDBBAwJbDEI&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=google
 
   require 'json'
   require 'httparty'
+  require 'pp'
 
   include HTTParty
 
   base_uri "http://api.glassdoor.com"
   API_ID = ENV["GLASSDOOR_API_ID"]
   API_KEY = ENV["GLASSDOOR_API_KEY"]
-
 
 
   def initialize
@@ -28,17 +29,27 @@ class CompanyProfiler
   def fetch_data(company)
     search = @options
     search[:query][:q] = company
-    @response = self.class.get("/api/api.htm", search)
+    full_response = self.class.get("/api/api.htm", search)
+    @response = full_response["response"]["employers"][0]
   end
 
 
-  def rating_overall
-    @response[:response][:employers][:overallRating]
+  def get_ratings
+    @ratings = {count: @response["numberOfRatings"],
+                overall: @response["overallRating"],
+                culture: @response["cultureAndValuesRating"],
+                leadership: @response["seniorLeadershipRating"],
+                compensation: @response["compensationAndBenefitsRating"],
+                opportunity: @response["careerOpportunitiesRating"],
+                balance: @response["workLifeBalanceRating"],
+                recommend: @response["recommendToFriendRating"]
+                }
   end
 
-# Example request ~ http://api.glassdoor.com/api/api.htm?t.p=38330&t.k=bDBBAwJbDEI&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=google
 
-
+  def featured_review
+    @review = @response["featuredReview"]
+  end
 
 
 end
