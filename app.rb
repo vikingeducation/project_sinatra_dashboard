0f387bb5cbdbb67e2ffd5119521c8be45ddabab5
David <davidmeza@outlook.com>
2015-07-14 23:35:52 -0400

    indeed scraper now working

--- app.rb
+++ app.rb
@@ -7,9 +7,14 @@ require_relative 'helpers/scraper.rb'
 
 helpers Scraper do
 
-  def run_scraper(job_title, location)
+  def run_dice_scraper(job_title, location)
     scraper = Scraper::DiceScraper.new(job_title, location, 2015, 06, 15)
     scraper.scrape
+  end
+
+  def run_indeed_scraper(job_title, location)
+    scraper = Scraper::IndeedScraper.new(job_title, location, 2015, 06, 15)
+    scraper.scrape_indeed
   end
 
 end
@@ -20,11 +25,21 @@ get '/' do
   erb :index
 end
 
-post '/scrape' do
+post '/scrape-dice' do
 
   job_title, location = params[:job_title], params[:location]
 
-  array = run_scraper(job_title, location)
+  array = run_dice_scraper(job_title, location)
+
+  erb :scrapes_table, :locals => { :job_title => job_title, :location => location, :array => array}
+
+end
+
+post '/scrape-indeed' do
+
+  job_title, location = params[:job_title], params[:location]
+
+  array = run_indeed_scraper(job_title, location)
 
   erb :scrapes_table, :locals => { :job_title => job_title, :location => location, :array => array}
 
