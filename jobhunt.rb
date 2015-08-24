d75ab8bcb0fc7bfc9e96e86af7a68c2776776f6e
Yusheng Chen <yschen9218@gmail.com>
2015-08-24 04:15:41 -0700

    defractor the methods into 2, still has some place to improve thanks to the glassdoor api

--- jobhunt.rb
+++ jobhunt.rb
@@ -1,13 +1,22 @@
 require 'sinatra'
+require 'json'
 require './helpers/dice.rb'
 require './helpers/gdhelper.rb'
+require './helpers/ip_helper.rb'
+require 'pry'
 
 helpers Dice_scraper
 helpers Glassdoorhelper
+helpers IPhelper
 
+enable :sessions
 
+#session[:emp_db] = {}
+#session[:ip] = nil
 get '/' do
-    erb :welcome
+    ip = IPhelper::Ipfinder.new
+    doc = "Your IP address is #{ip.ipjson["ip"]}"
+    erb :welcome, :locals => {:doc => doc, :city => ip.ipjson["city"]}
 end
 
 get '/search' do
@@ -15,6 +24,15 @@ get '/search' do
    location = params[:place]
    dice_job = Dice_scraper::Dice_tool.new(job,location)
    #job_list = dice_job.jobdb
-   erb :job_screen, :locals => {:job_list => dice_job.jobdb}
+   session[:employers] = dice_job.employers
+   erb :job_screen, :locals => {:job_list => dice_job.jobdb, :employer => dice_job.employers}
    #erb :test, :locals => {:job => job, :location => location}
 end
+
+get '/company' do
+  name = params[:company]
+  company = Glassdoorhelper::Gdhelper.new(name).gdresult
+  #binding.pry
+  erb :company, :locals => {:response => company}
+end
+
