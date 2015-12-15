ea9027b4553621960b76c9ab35ba4c9ad719c598
Sia <siakaramalegos@gmail.com>
2015-12-15 11:20:55 -0600

    Adds sinatra contrib, creates basic layout and search form

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-partial'
+gem 'sinatra-contrib'
 gem 'pry', group: :development
--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ require 'sinatra/reloader' if development?
 require 'erb'
 require 'json'
 require 'pry'
+
+# options for partials
+set :partial_template_engine, :erb
+enable :partial_underscores
+
+
+
+
+# Routes and controls
+get '/' do
+  erb :index
+end
