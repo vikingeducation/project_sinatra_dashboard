c8639120522c7895615c0780accf91f24e05539c
Thomas Lo <thomasjinlo@gmail.com>
2016-01-20 15:03:37 -0800

    Work on geolocation setup

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'thin'
 gem 'mechanize', '~>2.7.2', '<2.7.3'
-gem 'pry' # because you'll probably need it
+gem 'pry-byebug' # because you'll probably need it
 gem 'sinatra-contrib'
 gem 'bootstrap'
+gem 'httparty'
--- app.rb
+++ app.rb
@@ -8,11 +8,14 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require './helpers/scraper_helper.rb'
 require 'bootstrap'
+require 'httparty'
+require 'pry-byebug'
 
 helpers ScraperHelper
+enable :sessions
 
 get '/' do
-
+  get_api_location
   erb :index, locals: {results: nil}
 end
 
@@ -25,3 +28,9 @@ post '/' do
 
   erb :index, locals: {results: results}
 end
+
+def get_api_location
+  response = HTTParty.get("https://freegeoip.net/json/174.60.246.251")
+end
+
+# country_code region_code city
