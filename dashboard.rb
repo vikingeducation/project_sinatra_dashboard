6158791268c0618d798c79afcd981f1c1547abfa
koziscool <kozjob@yahoo.com>
2016-01-20 16:11:02 -0600

    created form, need to do custom query

--- dashboard.rb
+++ dashboard.rb
@@ -12,5 +12,13 @@ get '/' do
 end
 
 post '/' do
+  keywords = params[:keywords]
+  keywords = keywords.split("")
+  location = params[:location]
+
+  scraper = DiceScraper.new
+  scraper.scrape
+  job_array = CSV.read("dice_job.csv")
+  erb :index, locals: { job_table: job_array}
 
 end
