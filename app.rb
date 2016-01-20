1b253f7e36bdc331b9a3f8dbf2e2982e1b815bf1
Julia Herron <julia.herron@gmail.com>
2016-01-20 12:22:13 -0800

    less broken

--- app.rb
+++ app.rb
@@ -1,36 +1,40 @@
-require 'sinatra/base'
-# require 'sinatra/reloader' if development?
-require './helpers/scraper.rb'
+require 'sinatra'
 require 'thin'
 require 'erb'
 require 'mechanize'
+require 'chronic'
+require 'byebug'
+require 'csv'
+require 'sinatra/reloader' if development?
+require './helpers/scraper.rb'
+
 
 set :servers, ["thin", "puma", "webrick"]
 
 
 
 
-class MyApp < Sinatra::Base
+# class MyApp < Sinatra::Base
 
-  helpers DiceScraper
+#   helpers DiceScraper
 
-  get '/' do
-    erb :index
-  end
-end
+#   get '/' do
+#     erb :index
+#   end
+# end
 
 # MyApp.run!
 
 # helpers DiceScraper
 
 
-# get '/' do
-#   erb :index
-# end
+get '/' do
+  erb :index
+end
 
 
-# post '/' do
-#   search_term = params[:search]
-  # job_results = run(search_term)
-  # erb :dashboard, :locals => { :job_results => job_results}
-# end
+post '/' do
+  search_term = params[:search]
+  job_results = run(search_term)
+  erb :dashboard, :locals => { :job_results => job_results}
+end
