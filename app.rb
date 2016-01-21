ed6fbff7a6081fe56235c9ef25ad6e3bf8c1457a
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-20 20:27:25 -0500

    edit profiler

--- app.rb
+++ app.rb
@@ -18,13 +18,7 @@ helpers ScraperHelper
 enable :sessions
 
 get '/' do
-  if session['zip'].nil?
-    locator = Locator.new
-    locator.get_api_location
-    session['city'] = locator.city
-    session['region'] = locator.region
-    session['zip'] = locator.zip
-  end
+  set_session
 
   ua = request.user_agent
 
@@ -36,7 +30,28 @@ post '/' do
   time = Time.now - 12 * 3600
 
   results = scraper_helper(time, search_term, session['zip'])
+  results.each do |result|
+    company = result[2]
+    location = result[3]
+    set_profiler(company,location)
+  end
+
   erb :index, locals: {results: results, city: session['city'], region: session['region']}
+end
+
+def set_session
+  if session['zip'].nil?
+    locator = Locator.new
+    locator.get_api_location
+    session['city'] = locator.city
+    session['region'] = locator.region
+    session['zip'] = locator.zip
+  end
+end
+
+def set_profiler(company,location)
+  profiler = CompanyProfiler.new
+  profiler.create_uri(company,location)
 end
 
 # country_code region_code city
--- lib/company_profiler.rb
+++ lib/company_profiler.rb
@@ -6,18 +6,22 @@ Figaro.application = Figaro::Application.new(
 Figaro.load
 
 class CompanyProfiler
-
+  attr_accessor :uri
+  
   API_KEY = Figaro.env.API_KEY
   PARTNER_ID = Figaro.env.PARTNER_ID
   BASE_URI  = "http://api.glassdoor.com/api/api.htm?"
 
   def initialize
-    create_uri
   end
 
-  def create_uri
+  def create_uri(company,location)
     @uri = "#{BASE_URI}"
+    @uri << "v=1&format=json" # version and format
+    @uri << "&t.p=#{PARTNER_ID}&t.k=#{API_KEY}" # secret codes
+    @uri << "&action=employers&q=#{company}&l=#{location}" # search fields
+    # @uri << "&userip=#{ip}&useragent=#{ua}" # ip and user agent
+    binding.pry
   end
 
 end
-  
