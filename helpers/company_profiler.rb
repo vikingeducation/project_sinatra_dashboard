17818f7861a516ba7a57c92745f2beeb5819ad84
Kelsey James <kelseybjames@gmail.com>
2016-01-20 18:48:00 -0800

    TODO: Fix Figaro, figure out null returns

--- helpers/company_profiler.rb
+++ helpers/company_profiler.rb
@@ -1,25 +1,22 @@
 require 'httparty'
 require 'pp'
-require 'figaro'
 require_relative 'company'
 require_relative 'company_profiler_figaro'
-
 
 class CompanyProfiler
 
   attr_reader :company_info, :company
 
-  include HTTParty
-  base_uri 'http://api.glassdoor.com'
-
   GLASSDOOR_TP = ENV['glassdoor_tp']
   GLASSDOOR_TK = ENV['glassdoor_tk']
 
+  include HTTParty
+  base_uri 'http://api.glassdoor.com'
 
   def initialize(company_name)
     @company_name = company_name
+    puts GLASSDOOR_TP
     @options = { :query => { :v => 1, :format => 'json', :"t.p" => GLASSDOOR_TP, :"t.k" => GLASSDOOR_TK, :action => "employers", :q => @company_name }}
-
     company_ratings
   end
 
@@ -27,14 +24,14 @@ class CompanyProfiler
   def company_ratings
     @glassdoor_info = self.class.get("/api/api.htm?", @options)
     if @glassdoor_info["response"]["employers"][0]
-      stuff_we_want = @glassdoor_info["response"]["employers"][0]
-      @company = Company.new(overallRating: stuff_we_want["overallRating"], cultureAndValuesRating: stuff_we_want["cultureAndValuesRating"], compensationAndBenefitsRating: stuff_we_want["compensationAndBenefitsRating"], pros: "none", cons: "none")
+      @stuff_we_want = @glassdoor_info["response"]["employers"][0]
+      @company = Company.new(overallRating: @stuff_we_want["overallRating"], cultureAndValuesRating: @stuff_we_want["cultureAndValuesRating"], compensationAndBenefitsRating: @stuff_we_want["compensationAndBenefitsRating"], pros: "none", cons: "none")
       # pp stuff_we_want
     end
 
-    if stuff_we_want["featuredReview"]
-      @company.pros = stuff_we_want["featuredReview"]["pros"][0..200]
-      @company.cons = stuff_we_want["featuredReview"]["cons"][0..200]
+    if @stuff_we_want["featuredReview"]
+      company.pros = @stuff_we_want["featuredReview"]["pros"][0..200]
+      company.cons = @stuff_we_want["featuredReview"]["cons"][0..200]
     end
   end
 
@@ -45,6 +42,6 @@ end
 # http://api.glassdoor.com/api/api.htm?t.p=5317&t.k=n07aR34Lk3Y&userip=0.0.0.0&useragent=&format=json&v=1&action=employers
 
 
-# cp = CompanyProfiler.new("microsoft")
-# cp.company_ratings
-# pp cp.company
+cp = CompanyProfiler.new("microsoft")
+# pp cp.company_ratings
+# pp cp.stuff_we_want
