fc3a37bd091d14a9cede3cf0586c609ec43149ca
Vishal Lama <vishal.lama@gmail.com>
2016-07-21 12:08:40 -0600

    Update dashboard.rb

--- dashboard.rb
+++ dashboard.rb
@@ -1,11 +1,21 @@
 #!/usr/bin/env ruby
 
+require "bundler/setup"
 require "pry"
 require "sinatra"
 require "sinatra/reloader" if development?
 require "thin"
 require "mechanize"
+require_relative 'dice_scraper'
+
 
 get "/" do
+
+  unless Pathname.new("csv_file.csv").exists?
+    searcher = Scraper.new("developer", "raleigh, nc")
+    searcher.submit_form
+    searcher.write_to_csv(searcher.build_job_hash)
+  end
+
   erb :index
 end
