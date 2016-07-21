39be06f6feb9e1fd25011f690ee2286a275398c0
Leo Ahnn <leoahnn@gmail.com>
2016-07-21 17:21:22 -0400

    fixed helper, added location header

--- app.rb
+++ app.rb
@@ -1,21 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require './assignment_web_scraper/lib/scraper.rb'
+require './helpers/locator_helper.rb'
+
+puts "This is process #{Process.pid}"
 
 enable :sessions
 
+helpers LocatorHelper
 
-helpers Locator
-
-get '/' do 
-  session[:json] ||= Locator.new("108.51.25.16")
+get '/' do
+  session[:json] ||= LocatorHelper::Locator.send_request("108.51.25.16").run.response_body
   erb :index, locals: {position: nil, location: nil, json: session[:json]}
 
 end
 
-post '/' do
+post '/index' do
   position = params[:position]
   location = params[:location]
   limit = params[:limit]
-  erb :index, locals: {position: position, location: location, limit: limit}
+  erb :index, locals: {position: position, location: location, limit: limit, json: session[:json]}
 end
