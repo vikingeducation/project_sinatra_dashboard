e67309f8c512c834b94ffc0c8c9f137e2095f69d
Leo Ahnn <leoahnn@gmail.com>
2016-07-21 18:10:48 -0400

    worked on company profiler

--- helpers/company_profiler.rb
+++ helpers/company_profiler.rb
@@ -6,20 +6,36 @@ class CompanyProfiler
 
   def initialize(query)
     @query = query
-    @partner_id = PARTNER_ID
-    @api_key = API_KEY
+    @request = send_request
   end
 
   def send_request
-    Typhoeus::Request.new(build_uri, :method => :get)
+    request = Typhoeus::Request.new(build_uri, :method => :get)
+    JSON.parse(request.run.response_body)
   end
-
 
   def build_uri
-    "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=#{PARTNER_ID}&t.k=#{API_KEY}&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0"
-
+    "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=#{PARTNER_ID}&t.k=#{API_KEY}&action=employers&q=#{@query}"
   end
 
+  def find_job
+    @request["response"]["employers"].first
+  end
+
+  def featured_review
+    review = find_job["featuredReview"] if find_job["featuredReview"]
+    review
+  end
+
+  def ratings
+    job = find_job
+    ratings = {}
+    ratings["overall"] = job["overallRating"].to_s
+    ratings["culture_values"] = job["cultureAndValuesRating"]
+    ratings["compensation"] = job["compensationAndBenefitsRating"]
+    ratings["worklife"] = job["workLifeBalanceRating"]
+    ratings
+  end
 end
 
 
