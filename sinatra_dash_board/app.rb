d7df04bb599549a4d3a9913abcc9e6ccacfd8f7e
Alex Lach <alexglach@gmail.com>
2016-07-21 15:00:42 -0400

    search is working, need to style

---
+++ sinatra_dash_board/app.rb
@@ -0,0 +1,21 @@
+#app.rb
+
+require 'sinatra'
+require 'pry-byebug'
+require_relative '../dice_scraper/script'
+
+
+
+get '/index' do
+  erb :index
+end
+
+post '/search' do
+
+  scraper = DiceScraper.new(params[:job],params[:location])
+  arr = scraper.find_elements_and_return_links("div#search-results-experiment h3 .dice-btn-link")
+  scraper.get_all_company_info(arr)
+  jobs = scraper.jobs
+  erb :results, locals: {jobs: jobs}
+
+end
