d4f220c94a748076f35f769b978d2b23313181a9
Hannah Squier <hannahsquier@gmail.com>
2016-07-21 12:03:40 -0700

    create locator module

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require './helpers/job_helpers'
+require './helpers/locator_helper'
 
 helpers JobHelpers
+helpers Locator
 
 get '/' do
-  @ip = request.ip
-  erb :index
+  ip = request.ip
+  zip = Locator::Locator.new(request.ip).zip
+
+  erb :index, { locals: { zip: ip } }
 end
 
 post '/' do
