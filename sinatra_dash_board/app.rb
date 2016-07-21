7bbfe31a9f2669074bbf869bba2397a572a8a4a8
Alex Lach <alexglach@gmail.com>
2016-07-21 19:58:20 -0400

    finishing up

--- sinatra_dash_board/app.rb
+++ sinatra_dash_board/app.rb
@@ -18,6 +18,13 @@ post '/search' do
   arr = scraper.find_elements_and_return_links("div#search-results-experiment h3 .dice-btn-link")
   scraper.get_all_company_info(arr)
   jobs = scraper.jobs
+  jobs.each do |job|
+    c = CompanyProfiler.new(job[:company], job[:location])
+    job[:c_v] = c.get_company_info[0] 
+    job[:c_b] = c.get_company_info[1]
+    job[:pros] = c.get_company_info[2]
+    job[:cons] = c.get_company_info[3]
+  end
   erb :results, locals: {jobs: jobs}
 
 end
--- sinatra_dash_board/helpers.rb
+++ sinatra_dash_board/helpers.rb
@@ -1,4 +1,6 @@
 #helpers.rb
+require 'uri'
+require 'httparty'
 
 class Locator
   attr_reader :location
@@ -11,20 +13,31 @@ end
 class CompanyProfiler
   #Partner ID:  80591
   #Key:  f6KXrJi3ObW
+  END_POINT = "http://api.glassdoor.com/api/api.htm?t.p=80591&t.k=f6KXrJi3ObW&userip=192.168.43.42&useragent=chrome&format=json&v=1&action=employers"
 
-  def initialize(query)
-http://api.glassdoor.com/api/api.htm?t.p=80591&t.k=f6KXrJi3ObW&userip=192.168.43.42&useragent=chrome&format=json&v=1&action=employers&l=chicago&q=Hirewell
-    http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=80591&t.k=f6KXrJi3ObW&action=companies&q=pharmaceuticals&userip=192.168.43.42&useragent=Chrome/%2F4.0
-    http://api.glassdoor.com/api/api.htm?
-    v=1
-    &format=json&t.p=120
-    &t.k=fz6JLNDfgVs
-    &action=employers
-    &q=pharmaceuticals
-    &userip=192.168.43.42
-    &useragent=Mozilla
-    /%2F4.0
-    #
+  attr_reader :response
+
+  def initialize(company, location)
+    @q = URI.encode(company)
+    @l = URI.encode(location)
+    @response = HTTParty.get(build_url)
   end
+
+  def build_url
+    "#{END_POINT}&q=#{@q}&l=#{@l}"
+  end
+
+  def get_company_info
+    binding.pry
+    start = @response["response"]["employers"][0]
+    c_v_rating = start["cultureAndValuesRating"] || "N/A"
+    c_b_rating = start["compensationAndBenefitsRating"] || "N/A"
+    featured_pros = start["featuredReview"] || "N/A"
+    featured_cons = @response["response"]["employers"][0]["featuredReview"] || "N/A"
+    [c_v_rating, c_b_rating, featured_pros, featured_cons]
+  end
+
+  
+
 
 end
