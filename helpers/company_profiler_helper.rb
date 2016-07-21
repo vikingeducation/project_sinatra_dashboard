8e77bd1760421f8284afd8304256c0afe261cbbc
Hannah Squier <hannahsquier@gmail.com>
2016-07-21 14:44:15 -0700

    initialize CompanyProfiler module

---
+++ helpers/company_profiler_helper.rb
@@ -0,0 +1,14 @@
+require 'env_helper'
+
+module CompanyProfiler
+  class CompanyProfiler
+    def initialize(company)
+      BASE_URL : "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=#{$GLASSDOOR_ID}&t.k=#{$GLASSDOOR_KEY}&action=employers&userip=75.37.48.0&useragent="
+    end
+
+    def
+
+    end
+
+  end
+end
