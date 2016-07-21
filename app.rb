14ed32d78174b9ef1ad8f099c302b8f1e20d7347
Mike Lee <asackofwheat@gmail.com>
2016-07-21 14:44:37 -0400

    table works!

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require './dice_scraper.rb'
-require 'pry-byebug'
+# require 'pry-byebug'
 
 
 get '/' do 
@@ -12,6 +12,6 @@ post '/' do
   location = params[:location]
   scraper = DiceScraper.new(job_title, location)
   result_array = scraper.create_listings_array.compact
-  binding.pry
+  # binding.pry
   erb :results, locals: {:result_array => result_array}
 end
