fafb470a224110335bff10be17d9b99afde0fb0e
BranLiang <lby89757@hotmail.com>
2016-08-12 19:19:23 +0800

    update

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'mechanize'
 require "sinatra/reloader" if development?
 require 'pry-byebug'
 require './helpers/app_helper.rb'
+require 'csv'
 
 helpers AppHelper
 
@@ -10,4 +11,12 @@ enable :sessions
 
 get '/' do
   erb :index
+end
+
+post '/search' do
+
+  key = params[:key]
+  result_liebao = liebao key
+
+  erb :result, :locals => { key: key, result_liebao: result_liebao }
 end
