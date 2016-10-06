5b3f1a54494823ba7dc60096c36a31d999562c66
Joe Bernardi <joebernardi@hotmail.com>
2016-10-05 22:13:00 -0500

    set up files

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://www.rubygems.org'
+
+gem 'sinatra'
+gem 'mechanize', '~>2.7.2', '<2.7.3'
+gem 'pry'
+gem 'thin'
---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'json'
+require 'pry-byebug'
+
+# the core view layout shows the job listings in a table  (index.erb)
+# refreshing page starts job search
+# form for search query
+	# location ( city, state )
+	# job
+# job data to be displayed on table in page
+# use bootstrap
