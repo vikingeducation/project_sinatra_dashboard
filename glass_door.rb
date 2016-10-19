3c286dde41ee3ec22ba65b32c31a91d959022e04
Joe Bernardi <joebernardi@hotmail.com>
2016-10-18 20:26:09 -0400

    pull company ratings

--- glass_door.rb
+++ glass_door.rb
@@ -8,7 +8,7 @@ class GlassDoor
 
 	ID = ENV["GLASS_ID"]
 	KEY = ENV["GLASS_KEY"]
-	IP = "209.37.67.195"
+	IP = "67.235.56.2"
 
 
 	base_uri 'http://api.glassdoor.com/api/api.htm?v=1&format=json'
@@ -39,20 +39,38 @@ class GlassDoor
 
 	def get_reviews
 
-		@output = self.class.get( "", @options )
+		company = self.class.get( "", @options )
+
+		company['response']['employers'][0]
+
+=begin		{"id"=>2347,
+		 "name"=>"CDW",
+		 "website"=>"www.cdw.com",
+		 "isEEP"=>true,
+		 "exactMatch"=>true,
+		 "industry"=>"Computer Hardware & Software",
+		 "numberOfRatings"=>1250,
+		 "squareLogo"=>"https://media.glassdoor.com/sqll/2347/cdw-squarelogo.png",
+		 "overallRating"=>"3.9",
+		 "ratingDescription"=>"Satisfied",
+		 "cultureAndValuesRating"=>"4.2",
+		 "seniorLeadershipRating"=>"3.8",
+		 "compensationAndBenefitsRating"=>"3.4",
+		 "careerOpportunitiesRating"=>"3.8",
+		 "workLifeBalanceRating"=>"3.8",
+		 "recommendToFriendRating"=>82,
+		 "sectorId"=>10013,
+		 "sectorName"=>"Information Technology",
+		 "industryId"=>200060,
+		 "industryName"=>"Computer Hardware & Software",
+		 "featuredReview"=>
+		  {"attributionURL"=>
+		    "http://www.glassdoor.com/Reviews/Employee-Review-CDW-RVW12053166.htm",
+=end
 		binding.pry
 
 	end
 
-
-	def print_daily
-
-		 puts "Daily average for #{ @city_render }"
-		 day = 0
-
-		 @output["list"].each { |s| puts "Day #{day += 1} : #{s["temp"]["day"]}" }
-
-	end
 
 
 
