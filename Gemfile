45a339918169a7b351e0eb9aa2198cb96e206b04
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-16 13:34:32 -0500

    Begin Sinatra implementation

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 source "https://rubygems.org"
 
 # gem "rails"
-gem 'sinatra'
-gem 'thin'
+gem 'erb'
 gem 'mechanize'
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'thin'
--- dashboard.rb
+++ dashboard.rb
@@ -1,3 +1,17 @@
+#!/usr/bin/env ruby
 
+require 'erb'
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative './dice_scraper/lib/dice_scraper_controller'
+
+get '/' do
+  erb :index
+end
+
+post '/search_results' do
+  search_terms = params[:search_terms]
+  location     = params[:location]
+  erb :search_results, locals: { terms:    search_terms,
+                                 location: location }
+end
