2f400e14136d8b09346552f8520cbf0b32f2247f
James Harris <computersherpa@gmail.com>
2016-11-16 12:37:26 -0600

    Work on skeleton

--- app.rb
+++ app.rb
@@ -1,2 +1,10 @@
+#!/usr/bin/env ruby
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'helpers/dice_scraper_helper'
+
+helpers DiceScraperHelper
+
+get '/' do
+  "Welcome to our scraper! :D"
+end
---
+++ helpers/dice_scraper_helper.rb
@@ -0,0 +1,9 @@
+require_relative '../dice-scraper/lib/scraper'
+require_relative '../dice-scraper/lib/dice_scraper_controller'
+require_relative '../dice-scraper/lib/dice_jobs_page_parser'
+require_relative '../dice-scraper/lib/job_writer'
+require_relative '../dice-scraper/lib/dice_jobs_ui'
+
+module DiceScraperHelper
+
+end
