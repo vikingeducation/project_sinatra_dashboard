9c519f696c82b9af122f8c0e205d11bd0cc51f63
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-16 11:29:34 -0800

    switch

--- dashboard.rb
+++ dashboard.rb
@@ -4,13 +4,23 @@ require 'erb'
 require 'figaro'
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative './dice_scraper/lib/dice_scraper_controller'
+require_relative './assignment_web_scraper/lib/dice_scraper_controller'
+
+Figaro.application = Figaro::Application.new(
+  environment: 'development',
+  path: File.expand_path('../config/application.yml', __FILE__)
+)
+Figaro.load
+
+
+
 
 get '/' do
   erb :index
 end
 
-post '/search_results' do
+post '/' do
+
   search_terms = params[:search_terms]
   location     = params[:location]
   erb :search_results, locals: { terms:    search_terms,
