1898e58fe7ac59b8dc8c0d19d072c3905650aa32
James Harris <computersherpa@gmail.com>
2016-11-16 19:11:01 -0600

    Append data from glassdoor

--- classes/glassdoor.rb
+++ classes/glassdoor.rb
@@ -15,25 +15,39 @@ class CompanyProfiler
   attr_reader :user_ip, :user_agent
 
   def initialize( user_ip, user_agent )
-    p @user_ip = user_ip
-    p @user_agent = user_agent
+    @user_ip = user_ip
+    @user_agent = URI.escape(user_agent)
   end
 
   def get_comp_info(job)
     return {} if job[:company].nil? || job[:location].nil?
 
-    search( job[:company], job[:location])
+    results = search( job[:company], job[:location])
+    p results
+    parse_data(results)
+  end
+
+  def parse_data(results)
+    # results['response']['employers'][0]['overallRating']
+    {
+      overall_rating: results['response']['employers'][0]['overallRating'],
+      compensation_rating: results['response']['employers'][0]['compensationAndBenefitsRating'],
+      # industry: results['response']['employers'][0]['industryName'],
+      # pros: results['response']['employers'][0]['featuredReview']['pros'],
+      # cons: results['response']['employers'][0]['featuredReview']['cons']
+    }
   end
 
   # outputs a hash of values
   def search(company_name, city)
-    query = build_query(company_name, city)
-    p query
+    p query = build_query(URI.escape(company_name), URI.escape(city))
+    response = Net::HTTP.get("api.glassdoor.com", query)
+    JSON.parse(response)
   end
 
   def build_query(company, city)
-    query = "http://api.glassdoor.com/api/api.htm?"
-    query += "t.p=#{glassdoor_id}&t.k=#{glassdoor_key}"
+    query = "/api/api.htm?"
+    query += "t.p=#{ENV['glassdoor_id']}&t.k=#{ENV['glassdoor_key']}"
     query += "&userip=#{user_ip}&useragent=#{user_agent}"
     query += "&format=json&v=1&action=employers"
     query += "&q=#{company}&l=#{city}"
