0e3170e965b7c69128c15f36dd4df08fb949e5a3
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-20 16:09:05 -0800

    update

--- dice_scraper/lib/job_writer.rb
+++ dice_scraper/lib/job_writer.rb
@@ -7,11 +7,8 @@ class JobWriter
     headers = !File.exist?(filepath)
 
     CSV.open(filepath, 'a') do |csv|
-      # each one of these comes out in its own row.
-      csv << ["Job Title", "Company", "Link", "Location", "Posting Date", "Company ID", "Job ID"] if headers
 
-      results.each do |job|
-        csv << job.to_a
+#
       end
     end
   end
