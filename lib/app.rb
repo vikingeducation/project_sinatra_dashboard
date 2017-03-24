229778d9d78d407ff34de158a15b970c6f8a8fe7
efraimmgon <efraimmaugusto@gmail.com>
2017-03-24 07:22:47 -0400

    displaying the scrape

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,19 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require "erb"
+require_relative "scraper"
 
 enable :session
 
 get "/" do
-  "Hello world"
+  erb :index
+end
+
+post "/" do
+  scraper = Scraper.new
+  option = params[:scrape].to_i
+  result = scraper.scrape_first_page if option == 1
+  result = scraper.scrape_since(Time.now) if option == 2
+  locals = {locals: {result: result}}
+  erb :result, locals
 end
