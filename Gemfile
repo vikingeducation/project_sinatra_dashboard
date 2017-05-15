6d2cd140727a10d97bce047c229da64ec6aabd9d
Chad Lucas <chad.lucas.19@gmail.com>
2017-05-15 04:49:19 -0500

    update company profiler class

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'thin'
 gem 'openssl', '>=2.0.3'
 gem 'envyable'
 gem 'json'
+gem 'httparty'
 
 group :development do 
 	gem 'pry'
--- company_profiler.rb
+++ company_profiler.rb
@@ -2,19 +2,23 @@ require 'envyable'
 require 'mechanize'
 require 'json'
 require 'pry'
+require 'httparty'
 
 Envyable.load('config/env.yml')
 
 API_KEY = ENV["API_KEY"]
 ID = ENV["ID"]
-BASE_URI = "http://api.glassdoor.com"
+BASE_URI = "http://api.glassdoor.com/api/api.htm?"
 
 class CompanyProfiler
-	def initialize(company_name = "Google")
-		@agent = Mechanize.new
-		@agent.history_added = Proc.new {sleep 0.5}
+
+	include HTTParty
+
+	def initialize(company)
+		
 		@address = BASE_URI
 		@options = { :query => {
+														q: "#{company}",
 								 						v: "1",
 								 						format: "json",
 								 						useragent: "Chrome",
@@ -22,15 +26,17 @@ class CompanyProfiler
 								 						"t.p".to_sym => ID,
 								 						"t.k".to_sym => API_KEY
 			}}
-			@options[:query][:q] = company_name
+			
+		@glass_review = self.class.get(BASE_URI, @options)
 	end
 
-	def get_company_info
-		@agent.get(BASE_URI, @options) do |page|
-			puts page.body
-		end
+	def show_results
+		pp @glass_review.body
 	end
+
+
 end
 
-company = CompanyProfiler.new
-company.get_company_info
+company = CompanyProfiler.new("google")
+
+company.show_results
