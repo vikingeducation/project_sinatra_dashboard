373af84d727d77dcc44ac4e9e42dddaecf13c56d
Roy Chen <roychenlei@gmail.com>
2017-05-26 18:40:39 +0800

    required scraper file, modified root route

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra/reloader' if development?
 require 'erb'
 require 'pry-byebug'
 
+require './helpers/job_site_scraper'
+
 get '/' do
-  "The beginning of our great dashboard."
+  erb :home
 end
