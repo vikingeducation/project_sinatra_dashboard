4b8ab2071c83d04ad4240426e1daf61370939288
Roy Chen <roychenlei@gmail.com>
2017-05-27 15:38:58 +0800

    added post /search route

--- app.rb
+++ app.rb
@@ -8,3 +8,13 @@ require './helpers/job_site_scraper'
 get '/' do
   erb :index
 end
+
+post "/search" do
+  job_search_term = params[:job_search_term]
+  job_location = params[:job_location]
+
+  scraper = JobSiteScraper.new
+  job_postings = scraper.scrape_job_postings(job_search_term, job_location)
+
+  erb :index, locals: { job_postings: job_postings }
+end
