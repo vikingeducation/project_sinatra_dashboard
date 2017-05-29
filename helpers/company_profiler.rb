76afbc79bbf0d0846149fbf0696995c9e644710c
Roy Chen <roychenlei@gmail.com>
2017-05-29 18:52:19 +0800

    created CompanyProfiler class using HTTParty gem

---
+++ helpers/company_profiler.rb
@@ -0,0 +1,40 @@
+require 'httparty'
+require 'pp'
+
+class CompanyProfiler
+  include HTTParty
+
+  base_uri 'http://api.glassdoor.com/api/api.htm'
+  default_params v: '1',
+                 format: 'json',
+                 userip: '0.0.0.0',
+                 useragent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                 action: 'employers'
+
+  # initializes an instance of CompanyProfiler with
+  # my Glassdoor API partner ID and key
+  def initialize
+    config = load_config
+
+    self.class.default_params :'t.p' => config["t.p"], :'t.k' => config["t.k"]
+  end
+
+  # searches for company data
+  def search(options = {})
+    self.class.get(self.class.base_uri, options)
+  end
+
+  private
+
+  # loads my Glassdoor API partner ID and key from a YAML file
+  def load_config
+    YAML.load(File.read("#{File.dirname(__FILE__)}/company_profiler/company_profiler.yaml"))
+  end
+end
+
+if $0 == __FILE__
+  profiler = CompanyProfiler.new
+
+  query = { q: "Google", l: "Singapore"}
+  pp profiler.search(query: query)
+end
