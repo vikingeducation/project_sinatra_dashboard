896f53624ec13ebe2042f2dc39557637e1c9449a
andie5 <andieasa@googlemail.com>
2017-06-24 17:38:17 +0100

    job scraper formatted with boostrap complete

--- app.rb
+++ app.rb
@@ -24,13 +24,16 @@ post '/search' do
 
   job_scraper = JobScraper.new
 
-  postings = job_scraper.create_search(search_term, location, radius)
+  job_scraper.create_search(search_term, location, radius)
   job_scraper.extract_job_details
   job_scraper.csv_file.create_file(job_scraper.results)
+  postings = job_scraper.results
+
+  # session[:postings] = postings
 
     locals = {
-    :location: location,
-    :postings: postings
+    # location: location,
+    postings: postings
   }
 
   erb :index, :locals => locals
