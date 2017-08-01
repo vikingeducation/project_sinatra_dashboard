e4f7c1f6dc59ed58b1a5f3e1e0683c303f602b4d
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-08-01 16:41:41 -0600

    response body returning requested info, working on getting it to save properly

--- apiclient.rb
+++ apiclient.rb
@@ -12,12 +12,35 @@ require 'json'
 class APIClient
 
   def initialize(options={})
-    @partner_id, @partner_key, @userip, @useragent, @format, @v, @action, @base_uri, @q =
+    @partner_id, @partner_key, @userip, @useragent, @format, @v, @action, @base_uri =
     options[:partner_id], options[:partner_key], options[:userip], options[:useragent],
-    options[:format], options[:v], options[:action], options[:base_uri], options[:q]
+    options[:format], options[:v], options[:action], options[:base_uri]
   end
 
-  def send_request
+  def company_rating(company="Crescent Solutions, Inc.")
+    response = send_request(company)
+    response_body = JSON.parse(response.body)
+    # pp response_body
+    if response_body["response"]["totalRecordCount"] > 1
+      response_body["response"]["employers"].each do |hash|
+        if hash["name"] == company
+          response_body = hash
+        end
+      end
+    else
+      response_body = response_body["response"]["employers"]
+    end
+    puts "Overall Rating: #{response_body["overallRating"]}"
+    puts "Culture And Values Rating: #{response_body["cultureAndValuesRating"]}"
+    puts "Compensation And Benefits Rating: #{response_body["compensationAndBenefitsRating"]}"
+    puts "Worklife Balance Rating: #{response_body["workLifeBalanceRating"]}"
+    puts "Featured Reviews, Pros: #{response_body["featuredReview"]["pros"]}"
+    puts "Featured Reviews, Cons: #{response_body["featuredReview"]["cons"]}"
+  end
+
+
+
+  def send_request(company)
     uri = (@base_uri + "?")
     params = { "t.p" => @partner_id,
                "t.k" => @partner_key,
@@ -26,14 +49,16 @@ class APIClient
                "format" => @format,
                "v" => @v,
                "action" => @action,
-               "q" => @q,
+               "q" => company
              }
     request = Typhoeus::Request.new( uri,
                                      method: :get,
                                      params: params )
     request.run
-    pp request.response
+    request.response
   end
+
+
 
 
 end # end of class
