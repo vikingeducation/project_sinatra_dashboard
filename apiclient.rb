f601d839764d79dacba296b9ae4a9f5babe6330c
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-08-02 15:22:29 -0600

    info from scraper and info from glassdoor api now displaying on the same table

--- apiclient.rb
+++ apiclient.rb
@@ -3,10 +3,6 @@ require 'typhoeus'
 require 'httparty'
 require 'json'
 
-# http://api.glassdoor.com/api/api.htm?
-# t.p=177857&t.k=c0AkCTQ8MX5&userip=72.174.9.5&
-# useragent=chrome&format=json&v=1&action=employers&
-# q=Crescent Solutions Inc
 
 
 class APIClient
@@ -17,43 +13,51 @@ class APIClient
     options[:format], options[:v], options[:action], options[:base_uri]
   end
 
+
   def company_rating(company="Crescent Solutions, Inc.")
-    puts "#{company}"
     response = send_request(company)
     response_body = JSON.parse(response.body)
     pp response_body
-    if response_body["response"]["totalRecordCount"] > 1
-      response_body["response"]["employers"].each do |hash|
-        if hash["name"] == company
-          response_body = hash
-        end
+
+    if response_body["response"]["employers"].empty?
+      headers = ["Overall Rating", "Culture and Values Rating", "Comp. and Bene Ratings", "Worklife Balance", "Pros", "Cons"]
+      CSV.open('ratings.csv', 'a+', :headers => true) do |csv|
+        csv << headers if csv.count.eql? 0
+        csv << ["No Info available for this employer"]
       end
     else
-      response_body = response_body["response"]["employers"][0]
-    end
-    puts "+++++++++++++++++++++++"
-    puts "#{response_body}"
-    ratings = {:overall => response_body["overallRating"],
-               :culture_and_values => response_body["cultureAndValuesRating"],
-               :compensation_and_benefits => response_body["compensationAndBenefitsRating"],
-               :worklife_balance => response_body["workLifeBalanceRating"],
-              }
-    if response_body.has_key?("featuredReview")
-      ratings[:featured_review_pros] = response_body["featuredReview"]["pros"]
-      ratings[:featured_review_cons] = response_body["featuredReview"]["cons"]
-      puts "Featured Reviews, Pros: #{response_body["featuredReview"]["pros"]}"
-      puts "Featured Reviews, Cons: #{response_body["featuredReview"]["cons"]}"
-    end
-    puts "Overall Rating: #{response_body["overallRating"]}"
-    puts "Culture And Values Rating: #{response_body["cultureAndValuesRating"]}"
-    puts "Compensation And Benefits Rating: #{response_body["compensationAndBenefitsRating"]}"
-    puts "Worklife Balance Rating: #{response_body["workLifeBalanceRating"]}"
-    headers = ["Overall Rating", "Culture and Values Rating", "Comp. and Bene Rating", "Worklife Balance", "Pros", "Cons"]
-    CSV.open('ratings.csv', 'a+', :headers => true) do |csv|
-      csv << headers if csv.count.eql? 0
-      csv << ratings.values
-    end
+      if response_body["response"]["totalRecordCount"] > 1
+        response_body["response"]["employers"].each do |hash|
+          if hash["name"] == company
+            response_body = hash
+          end
+        end
+      else
+        response_body = response_body["response"]["employers"][0]
+      end
 
+      ratings = {:overall => response_body["overallRating"],
+                 :culture_and_values => response_body["cultureAndValuesRating"],
+                 :compensation_and_benefits => response_body["compensationAndBenefitsRating"],
+                 :worklife_balance => response_body["workLifeBalanceRating"],
+                }
+
+      if response_body.has_key?("featuredReview")
+        ratings[:featured_review_pros] = response_body["featuredReview"]["pros"]
+        ratings[:featured_review_cons] = response_body["featuredReview"]["cons"]
+        # puts "Featured Reviews, Pros: #{response_body["featuredReview"]["pros"]}"
+        # puts "Featured Reviews, Cons: #{response_body["featuredReview"]["cons"]}"
+      end
+      # puts "Overall Rating: #{response_body["overallRating"]}"
+      # puts "Culture And Values Rating: #{response_body["cultureAndValuesRating"]}"
+      # puts "Compensation And Benefits Rating: #{response_body["compensationAndBenefitsRating"]}"
+      # puts "Worklife Balance Rating: #{response_body["workLifeBalanceRating"]}"
+      headers = ["Overall Rating", "Culture and Values Rating", "Comp. and Bene Rating", "Worklife Balance", "Pros", "Cons"]
+      CSV.open('ratings.csv', 'a+', :headers => true) do |csv|
+        csv << headers if csv.count.eql? 0
+        csv << ratings.values
+      end
+    end
   end
 
 
--- app.rb
+++ app.rb
@@ -26,14 +26,13 @@ post '/search' do
   # options = search_params
   # find_jobs(options)
   @client = APIClient.new(PARAMETERS)
-  puts "--------------------------"
   puts "#{@client}"
   @csv_table = CSV.open("jobs.csv", :headers => true)
   @csv_table.each do |row|
     row.each do |element|
-      puts "ELEMENT: #{element}"
       if element[0] == "Company Name"
         @client.company_rating(element[1])
+        sleep rand(0..3)
       end
     end
   end
@@ -41,6 +40,14 @@ post '/search' do
 end
 
 get '/search' do
-  @csv_table = CSV.open("jobs.csv", :headers => true).read
+  @jobs_table = CSV.open("jobs.csv", :headers => true).read
+  @ratings_table = CSV.open("ratings.csv", :headers => true).read
+  @combined = @jobs_table.to_a.each_with_index.map {|row, index| row.to_a.concat(@ratings_table.to_a[index]) }
+  CSV.open('all.csv', "a+") do |row|
+    @combined.each do |line|
+      row << line
+    end
+  end
+  @csv_table = CSV.open("all.csv", :headers => true).read
   erb :results
 end
