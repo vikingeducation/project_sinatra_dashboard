c12757054d382995fc7f329b80d6033377aaf1d8
Sean Kelly <shonkelli@gmail.com>
2017-08-14 10:49:47 -0400

    could not register for api, added company_profiler class object but could not develop further

--- models/company_profiler.rb
+++ models/company_profiler.rb
@@ -1,5 +1,26 @@
 # /models/company_profiler.rb
+require 'rubygems'
+require 'mechanize'
+
+# CompanyProfiler
 class CompanyProfiler
+  def initialize(company_name = nil)
+    @company_name = company_name
+    @company_data = {}
+  end
+
+  # get_data
+  def get_data
+    # parses response hash from api and returns data when
+  end
+
+
+  # send_request
+  def send_request
+    # sends an request to the glassdoor api and returns a hash
+  end
+
+
 
 
 end
