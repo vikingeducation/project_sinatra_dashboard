d836be7bd8ea05ebd1379dd86da02b5e203f0521
anne <richardson.ae@gmail.com>
2017-10-31 20:08:02 -0500

    Puts placeholder data in '/' route

--- app.rb
+++ app.rb
@@ -13,14 +13,26 @@ enable :sessions
 
 get '/' do
   # Build objects
-  # scraper = Scraper.new('https://www.dice.com')
-  # scraper.scrape(title: 'Ruby on Rails Engineer', location: 'New Orleans, LA', name: 'Anne')
-  # scraper.scrape(title: 'Data Analyst Business Analyst', location: 'New Orleans, LA', name: 'Josh')
+  job1 = Job.new
+  job1.title = 'Super Amazing Job'
+  job1.job_id = '123'
+  job1.description_url = 'http://www.google.com'
+  job1.company = 'JazzHandRama'
+  job1.company_id ='254'
+  job1.location = 'Omaha'
+  job1.description = 'super job with fantastic happiness'
+  job1.date_posted = 'October Teenth'
+  job1.salary = '10,000,000'
+  job1.skills = 'ruby'
+  job1.remote = true
+
+  jobs = [job1]
 
   # Modify objects
 
   # Save objects to session
+  session['jobs'] = [job1, job1, job1, job1]
 
   # Output data to view
-  erb :index
+  erb :index, locals: { jobs: jobs }
 end
