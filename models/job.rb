091bf35269e8db8c3b5c050b6417c9ad67fdaa89
anne <richardson.ae@gmail.com>
2017-11-01 16:27:35 -0500

    Moves placeholder info into model to clean up app file

--- models/job.rb
+++ models/job.rb
@@ -14,4 +14,18 @@ class Job
     @remote = ''
   end
 
+  def add_placeholder_info
+    @title = 'Super Amazing Job'
+    @job_id = '123'
+    @description_url = 'http://www.google.com'
+    @company = 'JazzHandRama'
+    @company_id ='254'
+    @location = 'Omaha'
+    @description = 'super job with fantastic happiness'
+    @date_posted = 'October Teenth'
+    @salary = '10,000,000'
+    @skills = 'ruby'
+    @remote = true
+  end
+
 end
