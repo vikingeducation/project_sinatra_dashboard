4a861927539ed826d21693bdaf5990efc53c0ede
anne <richardson.ae@gmail.com>
2017-11-01 16:29:58 -0500

    Incorporates ip address-based location into form

--- app.rb
+++ app.rb
@@ -1,56 +1,56 @@
 require 'rubygems'
+require 'dotenv/load'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'erb'
+require 'csv'
 require 'pry'
 
 # Classes
 require './models/job.rb'
 require './models/scraper.rb'
+require './models/free_geo_api.rb'
 
 # Routes
 enable :sessions
 
 get '/' do
   # Build objects
-  job1 = Job.new
-  job1.title = 'Super Amazing Job'
-  job1.job_id = '123'
-  job1.description_url = 'http://www.google.com'
-  job1.company = 'JazzHandRama'
-  job1.company_id ='254'
-  job1.location = 'Omaha'
-  job1.description = 'super job with fantastic happiness'
-  job1.date_posted = 'October Teenth'
-  job1.salary = '10,000,000'
-  job1.skills = 'ruby'
-  job1.remote = true
+  settings.development? ? user_ip = ENV['EX_IP_ADDRESS'] : user_ip = request.ip
+  geo_api = FreeGeoAPI.new(user_ip)
+  location = geo_api.send_request
 
-  jobs = [job1]
+  placeholder_job = Job.new
 
   # Modify objects
+  placeholder_job.add_placeholder_info
+  jobs = [placeholder_job, placeholder_job]
 
   # Save objects to session
-  session['jobs'] = [job1, job1, job1, job1]
+  session['jobs'] = jobs
+  session['user_ip'] = user_ip
 
   # Output data to view
-  erb :index, locals: { jobs: jobs }
+  erb :index, locals: { keyword: nil, location: location, jobs: jobs, user_ip: user_ip }
 end
+
+# Prevent timeout while scraping dice.com
+set :server_settings, :timeout => 3600
 
 post '/search' do
   # Build objects
+  user_ip = session['user_ip']
   keyword = params['title_keyword']
   location = params['location']
   scraper = Scraper.new('https://www.dice.com')
 
   # Modify objects
-  matches = session['jobs']
-  # matches = scraper.scrape(title: keyword, location: location)
-  # scraper.scrape(title: 'Ruby on Rails Engineer', location: 'New Orleans, LA')
-  # scraper.scrape(title: 'Data Analyst Business Analyst', location: 'New Orleans, LA', name: 'Josh')
+  matches = scraper.scrape(title: keyword, location: location)
+  # jobs = CSV.parse("/Users/localflavor/Sites/rails_development/Viking/project_sinatra_dashboard/exports/jobs-2017-11-01 13:34:02 -0500.csv")
+  # jobs = session['jobs']
 
   # Save objects to session
 
   # Output data to view
-  erb :index, locals: { jobs: matches }
+  erb :index, locals: { keyword: keyword, location: location, jobs: matches, user_ip: user_ip }
 end
