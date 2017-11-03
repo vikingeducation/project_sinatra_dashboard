c6d3e1f1cfd02acdaded7fa6c1fbfdc536b5071f
anne <richardson.ae@gmail.com>
2017-11-03 10:16:11 -0500

    Refactors to set company info more cleanly

--- models/company.rb
+++ models/company.rb
@@ -6,37 +6,37 @@ require_relative 'review'
 
 class Company
 
-  attr_reader :id, :name, :location, :website, :number_of_ratings, :reviews
+  attr_reader :location
 
   def initialize(location, response)
-    @id = parse_id(response)
-    @name = parse_name(response)
+    @response = response
     @location = location
-    @website = parse_website(response)
-    @number_of_ratings = parse_number_of_ratings(response)
-    @reviews = []
-    parse_featured_review(response)
   end
 
-  def parse_id(response)
-    response['employers'][0]['id']
+  def id
+    @id ||= response['employers'][0]['id']
   end
 
-  def parse_name(response)
-    response['employers'][0]['name']
+  def name
+    @name ||= response['employers'][0]['name']
   end
 
-  def parse_website(response)
-    response['employers'][0]['website']
+  def website
+    @website ||= response['employers'][0]['website']
   end
 
-  def parse_number_of_ratings(response)
-    response['employers'][0]['number_of_ratings']
+  def number_of_ratings
+    @number_of_rating ||= response['employers'][0]['number_of_ratings']
   end
 
-  def parse_featured_review(response)
-    review_info = response['employers'][0]['featuredReview']
-    @reviews << Review.new(review_info)
+  def reviews
+    @reviews ||= begin
+      review_info = response['employers'][0]['featuredReview']
+      [Review.new(review_info)]
+    end
   end
+
+  private
+  attr_reader :response
 
 end
